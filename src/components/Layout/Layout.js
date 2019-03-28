c8447a15c7d774ddb1e4de0104c870da47c5472b
Olga Lavrenova <olga.lavrenova@jamaza.com>
2019-03-28 18:35:49 +0300

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Fragment>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
+    render () {
+        return (
+            <Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerToggleHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        );
+    }
+}
+
+export default Layout;
