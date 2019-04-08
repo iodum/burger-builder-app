e510735e16823fbcff7dd8c1de140ad78d8f5565
Olga Lavrenova <olga.lavrenova@jamaza.com>
2019-04-08 18:28:27 +0300

    saving auth token in local storage

--- src/App.js
+++ src/App.js
@@ -5,22 +5,52 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 
 class App extends Component {
-  render() {
-    return (
-      <Layout>
-          <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/logout" exact component={Logout} />
-              <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-      </Layout>
-    );
-  }
+    componentDidMount() {
+        this.props.onAuthCheckState();
+    }
+
+    render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path="/" exact component={BurgerBuilder} />
+                <Redirect to="/"/>
+            </Switch>
+        );
+        if ( this.props.isAuth ) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/logout" exact component={Logout} />
+                    <Route path="/" exact component={BurgerBuilder} />
+                    <Redirect to="/"/>
+                </Switch>
+            );
+        }
+        return (
+          <Layout>
+              { routes }
+          </Layout>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        isAuth: state.auth.token !== null
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuthCheckState: () => dispatch(actions.authCheckState())
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
