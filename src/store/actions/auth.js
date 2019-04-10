4b52000116acf9566be4928c39295157b3726afd
Olga Lavrenova <olga.lavrenova@jamaza.com>
2019-04-10 17:04:20 +0300

    rewrited auth actions via saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,4 @@
 import * as actionTypes from './actionTypes';
-import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -23,46 +22,30 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('userId');
-    localStorage.removeItem('expirationDate');
+    return {
+        type: actionTypes.AUTH_INITITATE_LOGOUT
+    }
+}
+
+export const logoutSucceed = () => {
     return {
         type: actionTypes.AUTH_LOGOUT
     }
 }
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
     }
 }
 
 export const auth = (email, password, isSignUp) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + process.env.REACT_APP_FIREBASE_KEY;
-        if ( !isSignUp ) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + process.env.REACT_APP_FIREBASE_KEY;
-        }
-        axios.post(url, authData)
-            .then(res => {
-                const expirationDate = new Date (new Date().getTime() + res.data.expiresIn*1000);
-                localStorage.setItem('token', res.data.idToken);
-                localStorage.setItem('userId', res.data.localId);
-                localStorage.setItem('expirationDate', expirationDate);
-                dispatch(authSuccess(res.data.idToken, res.data.localId));
-                dispatch(checkAuthTimeout(res.data.expiresIn));
-            })
-            .catch(err => {
-                dispatch(authFail(err.response.data.error));
-            });
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignUp: isSignUp
     }
 }
 
@@ -74,19 +57,7 @@ export const setAuthRedirectPath = (path) => {
 }
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if ( !token ) {
-            dispatch(logout());
-        } else {
-            const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if ( expirationDate <= new Date() ) {
-                dispatch(logout());
-            } else {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/1000));
-            }
-        }
+    return {
+        type: actionTypes.AUTH_CHECK_STATE
     }
 }
